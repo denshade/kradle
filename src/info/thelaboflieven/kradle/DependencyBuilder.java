aa3970ecb190b38e6fa5d2b851fd5e13a39e4434
denshade <lieven.vaneeckhaute@gmail.com>
2022-03-01 20:54:03 +0100

    Register speed.

--- src/info/thelaboflieven/kradle/DependencyBuilder.java
+++ src/info/thelaboflieven/kradle/DependencyBuilder.java
@@ -4,8 +4,10 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DependencyBuilder
@@ -17,23 +19,36 @@ public class DependencyBuilder
         } else {
             System.out.println("Found dependencies.txt");
         }
-        readLinesAndDownload(f, 1);
+        readLinesAndDownload(f);
     }
 
-    public static void readLinesAndDownload(File f, int nrThreads) throws IOException {
+    public static void readLinesAndDownload(File f) throws IOException {
         List<String> urls = Files.readAllLines(f.toPath());
+        List<TimingInfo> timingInfos = new ArrayList<>();
         urls.parallelStream().forEach((line) -> {
+            var timingInfo = new TimingInfo();
+            long start = System.currentTimeMillis();
             String[] lineParts = line.split(",");
-            downloadURL(lineParts[0], lineParts[1]);
+            try {
+                URL url = new URL(lineParts[0]);
+                File filename = new File(lineParts[1]);
+                downloadURL(url, filename);
+                long end = System.currentTimeMillis();
+                timingInfo.timing = end - start;
+                timingInfo.url = url;
+                timingInfo.filepath = filename;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            timingInfos.add(timingInfo);
         });
     }
 
-    private static void downloadURL(String urlPath, String filenamePath)
+    private static void downloadURL(URL urlPath, File filenamePath)
     {
-        File outputFile = new File(filenamePath);
-        System.out.println("Storing URL " + urlPath + " to " + outputFile.getAbsolutePath());
-        try (BufferedInputStream in = new BufferedInputStream(new URL(urlPath).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+        System.out.println("Storing URL " + urlPath + " to " + filenamePath.getAbsolutePath());
+        try (BufferedInputStream in = new BufferedInputStream(urlPath.openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream(filenamePath)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
---
+++ src/info/thelaboflieven/kradle/TimingInfo.java
@@ -0,0 +1,11 @@
+package info.thelaboflieven.kradle;
+
+import java.io.File;
+import java.net.URL;
+
+public class TimingInfo
+{
+    public long timing;
+    public URL url;
+    public File filepath;
+}
