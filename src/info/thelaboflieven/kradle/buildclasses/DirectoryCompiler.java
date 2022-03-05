83f05c1f0fab60644c2643a900abba99d9ef93e2
denshade <lieven.vaneeckhaute@gmail.com>
2022-03-05 17:40:06 +0100

    DirectoryCompiler

--- src/info/thelaboflieven/kradle/buildclasses/DirectoryCompiler.java
+++ src/info/thelaboflieven/kradle/buildclasses/DirectoryCompiler.java
@@ -1,9 +1,26 @@
 package info.thelaboflieven.kradle.buildclasses;
 
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class DirectoryCompiler
 {
-    public static void compile()
-    {
 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        compile(new File("src"));
+    }
+    public static void compile(File source) throws IOException, InterruptedException {
+        List<String> commandLine = JavaSourceFileFinder.findJavaSources(source).stream().map(File::getAbsolutePath).collect(Collectors.toList());
+        commandLine.add(0, "javac.exe");
+        Process p = Runtime.getRuntime().exec(commandLine.toArray(new String[0]), new String[0], new File("classes"));
+        p.waitFor();
+        BufferedReader stdError = new BufferedReader(new
+                InputStreamReader(p.getErrorStream()));
+        BufferedReader stdOut = new BufferedReader(new
+                InputStreamReader(p.getInputStream()));
+        System.out.println(stdError.lines().collect(Collectors.joining()));
+        System.out.println(stdOut.lines().collect(Collectors.joining()));
     }
 }
