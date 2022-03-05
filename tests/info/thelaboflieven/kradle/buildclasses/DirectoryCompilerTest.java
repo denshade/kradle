55156236a431f8d82f912ff6145b293ab0c6b82b
denshade <lieven.vaneeckhaute@gmail.com>
2022-03-05 20:27:43 +0100

    DirectoryCompiler

--- tests/info/thelaboflieven/kradle/buildclasses/DirectoryCompilerTest.java
+++ tests/info/thelaboflieven/kradle/buildclasses/DirectoryCompilerTest.java
@@ -2,15 +2,22 @@ package info.thelaboflieven.kradle.buildclasses;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DirectoryCompilerTest {
 
     @Test
-    void testCompile()
-    {
-        DirectoryCompiler.compile();
-
+    void testCompile() throws IOException, InterruptedException {
+        String tmpdir = System.getProperty("java.io.tmpdir");
+        File classes = new File(tmpdir+File.separator+"classes");
+        classes.mkdirs();
+        DirectoryCompiler.compile(new File("src"), classes);
+        var files = classes.listFiles();
+        assertTrue(files.length > 0);
+        classes.delete();
     }
 
 }
