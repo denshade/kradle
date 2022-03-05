3156041200f381e063050ddcd307177e961c7d6a
denshade <lieven.vaneeckhaute@gmail.com>
2022-03-05 17:14:20 +0100

    Add JavaSourceFileFinder

--- src/info/thelaboflieven/kradle/buildclasses/JavaSourceFileFinder.java
+++ src/info/thelaboflieven/kradle/buildclasses/JavaSourceFileFinder.java
@@ -3,13 +3,14 @@ package info.thelaboflieven.kradle.buildclasses;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class JavaSourceFileFinder
 {
     public static List<File> findJavaSources(File sourceDirectory)
     {
         List<File> files = new ArrayList<>();
-        for (File file : sourceDirectory.listFiles())
+        for (File file : Objects.requireNonNull(sourceDirectory.listFiles()))
         {
             if (file.isDirectory()) {
                 files.addAll(findJavaSources(file));
